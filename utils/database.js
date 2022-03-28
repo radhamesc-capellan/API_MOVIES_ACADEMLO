22ec88fabeebe0b035fa63219c8cd87fe15fab69
Radhames Capellan <radhamesc@gmail.com>
2022-03-28 04:34:44 -0500

    modify database file

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: '../config.env' });
 
 const sequelize = new Sequelize({
     host: process.env.DB_HOST,
